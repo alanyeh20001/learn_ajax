7c421c5555e83a20b14f4081e7061940bce5bc14
alanyeh20001 <alanyeh20001@hotmail.com>
2016-08-24 21:56:35 +0800

    create member model & set route

---
+++ app/models/member.rb
@@ -0,0 +1,6 @@
+class Member < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+end
