36f2b4ef772e1a7b22539ea675a95cb5fafe85a8
alanyeh20001 <alanyeh20001@hotmail.com>
2016-08-29 00:52:46 +0800

    complete ajax response of registration form

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,3 +10,18 @@ $(function () {
     $.magnificPopup.close();
   });
 });
+
+// Show signup error messages in popup modal
+$(document).on("submit", "#registration_form", function(e) {
+
+}).on("ajax:success", "#registration_form", function(e, data, status, xhr) {
+  console.log("success: #{data}")
+}).on("ajax:error", "#registration_form", function(e, data, status, xhr) {
+  $(".alert_info").empty();
+
+  for (var key in data.responseJSON) {
+    console.log(key, data.responseJSON[key])
+    $(".alert_info").append("<p>"+key+" "+data.responseJSON[key][0]+"</p>");
+    $(".alert_info").css("background-color", "#F88E8B");
+  }
+});
